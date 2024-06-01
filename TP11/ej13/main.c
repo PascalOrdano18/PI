fe7cc5ea6318bafa3458ad069e00cce27c56484e
PascalOrdano18 <pascalordanodev@gmail.com>
2024-06-01 20:55:39 -0300

    rankingADT: OK

--- TP11/ej13/main.c
+++ TP11/ej13/main.c
@@ -17,26 +17,26 @@ int main(void) {
   addRanking(r, "Ride of the Valkyries");
   addRanking(r, "Yesterday");
   addRanking(r, "Ride of the Valkyries");
-
+  puts("ok");
   const char * aux = "default";
   int n;
   n = getByRanking(r, 6, &aux);
   assert( n == 0 && !strcmp(aux, "default") );
 
-  n = getByRanking(r, 1, &aux); 
+  n = getByRanking(r, 1, &aux);
   assert( n == 1 && !strcmp(aux, "Yesterday") );
 
-  n = getByRanking(r, 0, &aux); 
+  n = getByRanking(r, 0, &aux);
   assert( n == 0 && !strcmp(aux, "Yesterday") );
 
-  n = getByRanking(r, 2, &aux); 
+  n = getByRanking(r, 2, &aux);
   assert( n == 1 && !strcmp(aux, "Footloose") );
 
-  n = getByRanking(r, 1, &aux); 
+  n = getByRanking(r, 1, &aux);
   assert( n == 1 && !strcmp(aux, "Footloose") );
- 
-  assert( contains(r, "Ride of the Valkyries") ); 
-  assert( contains(r, "Sucio y desprolijo") ); 
+
+  assert( contains(r, "Ride of the Valkyries") );
+  assert( contains(r, "Sucio y desprolijo") );
   assert( contains(r, "Desarma y sangra") == 0 );
 
   size_t dimTop = 10;
@@ -53,37 +53,36 @@ int main(void) {
 
   free(top);
   freeRanking(r);
-  
+
   // Creamos un ranking con miles de elementos
   int qty = 5000;
   char * songs[qty];
-  
+
   for(int i=0; i<qty; i++) {
 	  songs[i] = malloc(100);
 	  sprintf(songs[i], "Cancion %04d", i+1);
   }
-  r = newRanking(songs, qty, strcmp);  
+  r = newRanking(songs, qty, strcmp);
   assert(size(r)==qty);
-  
+
   assert(position(r, "Cancion 0001")==1);
   assert(position(r, "Cancion 4500")==4500);
-  
-  assert( contains(r, "Cancion 0001") ); 
+
+  assert( contains(r, "Cancion 0001") );
   assert(position(r, "Cancion 0001")==1);
-  assert( contains(r, "Cancion 1234") ); 
+  assert( contains(r, "Cancion 1234") );
   assert(position(r, "Cancion 1234")==1233);
   assert(position(r, "Cancion 1233")==1234);
   assert(position(r, "Cancion 5000")==5000);
   assert(position(r, "Cancion xxx")==0);
   assert(size(r)==qty);
-  
-  
+
+
   for(int i=0; i<qty; i++) {
 	  free(songs[i]);
   }
   freeRanking(r);
-  
+
   puts("rankingADT: OK!");
   return 0;
 }
-
--- TP11/ej13/rankingADT.c
+++ TP11/ej13/rankingADT.c
@@ -4,125 +4,104 @@
 #include <ctype.h>
 #include "rankingADT.h"
 
-
-#define BLOCK 20
-
-typedef elemType* rankingArr;
-
-
 struct rankingCDT{
-    rankingArr rank;
+    elemType* rank;
     compare cmp;
     size_t size;
 };
 
-
 static void swapRanks(rankingADT ranking, int pos);
-
 
 rankingADT newRanking(elemType elems[], size_t dim, compare cmp){
   rankingADT newR = malloc(sizeof(struct rankingCDT));
   newR->size = dim;
   newR->cmp = cmp;
-  newR->rank = malloc(dim * sizeof(elemType));
 
-  if(dim != 0){
-    for(int i = 0; i < dim; i++){
-      strcpy(newR->rank[i], elems[i]);
+  if(dim > 0){
+      newR->rank = malloc(dim * sizeof(elemType));
+      for(int i = 0; i < dim; i++){
+          newR->rank[i] = elems[i];
     }
+  }
+  else{
+      newR->rank = NULL;
   }
   return newR;
 }
 
 
 static void swapRanks(rankingADT ranking, int pos){
-  int index = pos - 1;
-  elemType aux = ranking->rank[index];
-  ranking->rank[index] = ranking->rank[index-1];
-  ranking->rank[index-1] = aux;
+    if(pos == 1 || pos > ranking->size)
+        return ;
+    int index = pos - 1;
+    elemType aux = ranking->rank[index];
+    ranking->rank[index] = ranking->rank[index-1];
+    ranking->rank[index-1] = aux;
 }
 
-
-
 void addRanking(rankingADT ranking, elemType elem){
-  int pos = position(ranking, elem);
-  
-  if(pos != 0 && pos != 1){   // Significa que esta en el ranking y no es el Top 1
-    swapRanks(ranking, pos);
+    int pos = position(ranking, elem);
+    if(pos != 0){   // Significa que esta en el ranking y no es el Top 1
+        swapRanks(ranking, pos);
+        return ;
+    }
+    ranking->rank = realloc(ranking->rank, (ranking->size+1) * sizeof(elemType));
+    ranking->rank[ranking->size] = elem;
+    ranking->size++;
     return ;
-  }
-   // Significa que no estaba en el ranking -> lo agrego al final
-  if(ranking->size % BLOCK == 0){
-    ranking->rank = realloc(ranking->rank, (ranking->size + BLOCK) * sizeof(elemType));
-  }
-  strcpy(ranking->rank[ranking->size++], elem);
-  return ;
 }
 
 
 size_t size(const rankingADT ranking){
-  return ranking->size;
+    return ranking->size;
 }
 
 int position(const rankingADT r, elemType elem){
-  for(int i = 0; i < r->size; i++){
-    if(r->cmp(r->rank[i], elem) == 0){
-      return i + 1;   // Pues i es el indice pero necesito la posicion en el ranking, que empieza en 1
+    for(int i = 0; i < r->size; i++){
+        if(r->cmp(r->rank[i], elem) == 0)
+        return i + 1;   // Pues i es el indice pero necesito la posicion en el ranking, que empieza en 1
     }
-  }
-  return 0;  // Si no encontro nada, retorna cero
+    return 0;  // Si no encontro nada, retorna cero
 }
-
 
 int getByRanking(rankingADT ranking, size_t n, elemType* elem){
-  if(n > ranking->size){
-    return 0;
-  }
-  int index = n-1;
-  *elem = ranking->rank[index];
-  swapRanks(ranking, n);
-  return 1;
+    int index = n-1;
+    if(n > ranking->size || n < 1)
+        return 0;
+    *elem = ranking->rank[index];
+    if(n > 1)
+        swapRanks(ranking, n);
+    return 1;
 }
 
-
-
 elemType* getTopRanking(const rankingADT ranking, size_t* top){
-  if(ranking->size = 0){
-    *top = 0;
-    return NULL;
-  }
-  
-  if(*top > ranking->size){
-    *top = ranking->size;
-    return ranking->rank;
-  }
-
-  elemType topArr[*top];
-  for(int i = 0; i < *top; i++){
-    strcpy(topArr[i], ranking->rank[i]);
-  }
-  return topArr;
+    if(ranking->size == 0){
+        *top = 0;
+        return NULL;
+    }
+    if(*top > ranking->size)
+        *top = ranking->size;
+    elemType* topArr = malloc(*top * sizeof(elemType));
+    for(int i = 0; i < *top; i++)
+        topArr[i] = ranking->rank[i];
+    return topArr;
 }
 
 int contains(rankingADT ranking, elemType elem){
-  int pos = position(ranking, elem);
-  if(pos == 0){
-    return 0;
-  }
-  swapRanks(ranking, pos);
-  return 1;
+    int pos = position(ranking, elem);
+    if(pos == 0)
+        return 0;
+    if(pos != 1)
+        swapRanks(ranking, pos);
+    return 1;
 }
 
-
 void downByRanking(rankingADT ranking, size_t n){
-  if(n < ranking->size){
-    swapRanks(ranking, n+1);
-  }
+    if(n < ranking->size)
+        swapRanks(ranking, n+1);
 }
 
 void freeRanking(rankingADT r){
-  free(r->rank);
-  free(r);
+    free(r->rank);
+    free(r);
 }
-
-
