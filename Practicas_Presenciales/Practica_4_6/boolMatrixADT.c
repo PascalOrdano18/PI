5cd4b6366b6106f2e375b071a0fccb349592cdcd
PascalOrdano18 <pascalordanodev@gmail.com>
2024-06-04 17:46:02 -0300

    Ultima clase de PI

---
+++ Practicas_Presenciales/Practica_4_6/boolMatrixADT.c
@@ -0,0 +1,89 @@
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolMatrixADT.h"
+
+struct boolCol{
+    size_t colNumber;
+    struct boolCol* tail;
+};
+
+
+typedef struct boolCol* boolColList;
+
+struct boolRowNode{
+    size_t rowNumber;
+    boolColList cols;
+    struct boolRowNode* tail;
+};
+
+typedef struct boolRowNode* boolRowList;
+
+
+struct boolMatrixCDT{
+    boolRowList rows;
+};
+
+boolMatrixADT newBoolMatrix(void){
+    return calloc(1, sizeof(struct boolMatrixCDT));
+}
+
+static boolRowList getRowRec(boolRowList l, size_t row){
+    if(l == NULL || l->rowNumber > row)
+        return NULL;
+    if(l->rowNumber == row)
+        return l;
+    return getRowRec(l->tail, row);
+}
+
+
+
+static boolColList getColRec(boolColList l, size_t col){
+    if(l == NULL || l->colNumber > col)
+        return NULL;
+    if(l->colNumber == col)
+        return l;
+    return getColRec(l->tail, col);
+}
+
+
+
+
+
+
+boolean getValue(const boolMatrixADT m, size_t row, size_t col){
+
+}
+
+static boolColList setColRec(boolColList l, size_t col, boolean value){
+    if(l == NULL || l->colNumber )
+}
+
+static boolRowList setRowRec(boolRowList l, size_t row, size_t col, boolean value){
+    if(l == NULL || l->rowNumber < row){
+        if(value == true){
+            boolRowList newRow = malloc(sizeof(*newRow));
+            newRow->rowNumber = row;
+            newRow->tail = l;
+            newRow->cols = setColRec(NULL, col, value);
+        }
+        return l;
+    }
+    if(l->rowNumber == row){
+        l->cols = setColRec(l->cols, col, value);
+        if(l->cols == NULL){
+            boolRowList aux = l->tail;
+            free(l);
+            return aux;
+        }
+        return l;
+    }
+    l->tail = setRowRec(l->tail, row, col, value);
+    return l;
+}
+
+
+void setValue(boolMatrixADT m, size_t row, size_t col, boolean value){
+    m->rows = setRowRec(m->rows, row, col, value);
+}
---
+++ Practicas_Presenciales/Practica_4_6/boolMatrixADT.h
@@ -0,0 +1,42 @@
+/**
+ * Se desea implementar una matriz booleana "infinita" (cada elemento es 1 ó 0) pero se sabe que la gran mayoría de los
+ * elementos tienen el valor 0 (falso).
+ * Para ahorrar espacio se usa una multilista, donde la lista principal o "troncal" representa las filas y cada sublista
+ * representa las columnas. Ambas listas están ordenadas en forma ascendente.
+ * Una multilista vacía se representa con el valor NULL
+ *
+ * Ejercicio de Parcial 1C Año 2011
+ */
+#include <stdlib.h>
+
+typedef struct boolMatrixCDT * boolMatrixADT;
+
+typedef enum {false=0, true} boolean;
+
+/*
+ * Crea una matriz con todos los elementos en falso
+ */
+boolMatrixADT newBoolMatrix(void);
+
+/*
+** Si @value es true entonces m[row][col] = value
+** Si @value es false entonces borra m[row][col]
+*/
+void setValue(boolMatrixADT m, size_t row, size_t col, boolean value);
+
+/*
+ * Retorna el valor almacenado en m[row][col]
+ */
+boolean getValue(const boolMatrixADT m, size_t row, size_t col);
+
+/*
+ * Libera toda la memoria reservada
+ */
+void freeBoolMatrix(boolMatrixADT m);
+
+/*
+ * OTRA VERSIÓN: ¿Qué habría que cambiar si se agrega la función negate?
+ *
+ * Niega el valor de cada una de las celdas. m[i][j] = !m[i][j] para todo i,j
+ */
+void negate(boolMatrixADT m);
